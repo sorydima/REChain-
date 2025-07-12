265e6d13dd7d1c4d65de768da503d979989a861d
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-07-12 18:48:25 +0300

    diff ..

--- lib/telegram_web_app_stub.dart
+++ lib/telegram_web_app_stub.dart
@@ -4,7 +4,7 @@ class TelegramWebApp {
   String? get backgroundColor => null;
   String? get version => null;
   String? get platform => null;
-  String? get colorScheme => null;
+  _ColorScheme? get colorScheme => null;
   bool get isActive => false;
   bool get isExpanded => false;
   double get viewportHeight => 0.0;
@@ -14,8 +14,21 @@ class TelegramWebApp {
   dynamic get initData => null;
   dynamic get initDataUnsafe => null;
   bool get isVerticalSwipesEnabled => false;
+  dynamic get themeParams => null;
+  dynamic get headerColor => null;
+  dynamic get bottomBarColor => null;
 
   void expand() {}
   void enableVerticalSwipes() {}
   void disableVerticalSwipes() {}
+}
+
+class _ColorScheme {
+  final String name = '';
+}
+
+class TelegramThemeUtil {
+  static dynamic getTheme(TelegramWebApp app) {
+    return null;
+  }
 }
