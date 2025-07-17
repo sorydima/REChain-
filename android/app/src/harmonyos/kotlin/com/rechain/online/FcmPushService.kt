9a3b83265dc0ab4daf2ef39c21d5cc185c5e35a1
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-07-17 15:42:07 +0300

    diff ..

---
+++ android/app/src/harmonyos/kotlin/com/rechain/online/FcmPushService.kt
@@ -0,0 +1,28 @@
+/*package com.rechain.online
+
+import com.famedly.fcm_shared_isolate.FcmSharedIsolateService
+
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.dart.DartExecutor.DartEntrypoint
+import android.content.Context
+
+class FcmPushService : FcmSharedIsolateService() {
+    override fun getEngine(): FlutterEngine {
+        return provideEngine(getApplicationContext())
+    }
+
+    companion object {
+        fun provideEngine(context: Context): FlutterEngine {
+            var engine = MainActivity.engine
+            if (engine == null) {
+                engine = MainActivity.provideEngine(context)
+                engine.getLocalizationPlugin().sendLocalesToFlutter(
+                    context.getResources().getConfiguration())
+                engine.getDartExecutor().executeDartEntrypoint(
+                    DartEntrypoint.createDefault())
+            }
+            return engine
+        }
+    }
+}
+*/
